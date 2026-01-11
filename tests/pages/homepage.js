a853f76253569849fe7533817827b624855ca075
mharalikar-elevate <mugdhah@umich.edu>
2026-01-11 12:41:15 -0500

    POM skeleton with home test page

---
+++ tests/pages/homepage.js
@@ -0,0 +1,22 @@
+import {Page} from "@playwright/test";
+
+class HomePage{
+
+   /**
+     * @param {import('@playwright/test').Page} page
+     */
+    constructor(page) {
+        this.page = page;
+        this.signInLink = page.getByRole('link', {name:"Sign In"});
+    }
+
+    async goTo(){
+        await this.page.goto("https://github.com/")
+    }
+
+    async getButtonContentHomepage(){
+        await this.signInLink.innerText();
+    }
+}
+
+module.exports = {HomePage};
